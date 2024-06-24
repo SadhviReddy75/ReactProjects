7648ca74a79c3d31c4747e3bac5d9677568d3de4
SadhviReddy75 <sadhvireddybuddannagari@gmail.com>
2024-06-24 15:10:16 +0530

    Add files via upload

---
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -0,0 +1,25 @@
+
+
+
+export default function GameBoard({onSelectSquare, turns}) {
+    
+    
+
+    return (
+        <ol id="game-board">
+            {turns.map((row, rowIndex) => (
+                <li key = {rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol,colIndex) => (
+                            <li key = {colIndex}>
+                                <button onClick = {()=> onSelectSquare(rowIndex,colIndex)} disabled = {playerSymbol !== null}>
+                                    {playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
+
+    );
+}
